d93c6379ec9665352e4ecab3985593fe81d4ea8f
Kavishka Roshan <weerawardhanakr@gmail.com>
2025-01-14 21:08:27 +0530

    Tab page developed

--- app/(screens)/(tabs)/_layout.tsx
+++ app/(screens)/(tabs)/_layout.tsx
@@ -1,61 +1,61 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router';
-import { MaterialCommunityIcons} from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 const TabRootLayout = () => {
   return (
     <>
-     <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "blue",
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => {
-              return   <MaterialCommunityIcons name="home" size={24} color= {color}/>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#1E90FF",
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => {
+              return <MaterialCommunityIcons name="home" size={24} color={color} />
             },
-            headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="fovourites"
-        options={{
-          title: 'Fovourites',
-          tabBarIcon: ({ color, focused }) => {
-            return (
-              <MaterialCommunityIcons name="heart" size={24} color= {color}/>
-            );
-          }
-        }}
-      />
-      <Tabs.Screen
-        name="setting"
-        options={{
-          title: 'Setting',
-          tabBarIcon: ({ color, focused }) => {
-            return (
-              <MaterialCommunityIcons name="cog" size={24} color= {color}/>
-            );
-          }
-          
-        }}
-      />
-      <Tabs.Screen
-        name="notifications"
-        options={{
-          title: 'Notifications',
-          tabBarIcon: ({ color, focused }) => {
-            return (
-              <MaterialCommunityIcons name="message" size={24} color= {color}/>
-            );
-          }
-        }}
-      />
-    </Tabs>
+            // headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="fovourites"
+          options={{
+            title: 'Fovourites',
+            tabBarIcon: ({ color, focused }) => {
+              return (
+                <MaterialCommunityIcons name="heart" size={24} color={color} />
+              );
+            }
+          }}
+        />
+        <Tabs.Screen
+          name="messages"
+          options={{
+            title: 'Notifications',
+            tabBarIcon: ({ color, focused }) => {
+              return (
+                <MaterialCommunityIcons name="message" size={24} color={color} />
+              );
+            }
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: 'Setting',
+            tabBarIcon: ({ color, focused }) => {
+              return (
+                <MaterialCommunityIcons name="cog" size={24} color={color} />
+              );
+            }
+
+          }}
+        />
+      </Tabs>
     </>
   )
 }

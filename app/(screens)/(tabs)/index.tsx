f913d9d67b9a290c9429448cdf5bb30014ed7d09
Kavishka Roshan <weerawardhanakr@gmail.com>
2025-01-18 12:33:04 +0530

    Add services list

--- app/(screens)/(tabs)/index.tsx
+++ app/(screens)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView ,Dimensions,Platform, StyleSheet, TouchableOpacity, TextInput, Image, useWindowDimensions, ScrollView} from 'react-native'
+import { View, Text, SafeAreaView, Dimensions, Platform, StyleSheet, TouchableOpacity, TextInput, Image, useWindowDimensions, ScrollView } from 'react-native'
 import React from 'react'
 import { useFonts } from 'expo-font';
 import { Ionicons, FontAwesome5, MaterialIcons, EvilIcons, FontAwesome } from '@expo/vector-icons';
@@ -8,47 +8,47 @@ import { DrawerActions, useNavigation } from '@react-navigation/native';
 // data image
 const slides = [
   {
-    image: require("../../../assets/images/travel/1.jpg"),
+    image: require("../../../assets/images/PC/1.jpg"),
   },
   {
-    image: require("../../../assets/images/travel/2.jpg"),
+    image: require("../../../assets/images/PC/2.jpg"),
   },
   {
-    image: require("../../../assets/images/travel/3.jpg"),
+    image: require("../../../assets/images/PC/3.jpg"),
   },
   {
-    image: require("../../../assets/images/travel/4.jpg"),
+    image: require("../../../assets/images/PC/4.jpg"),
   },
   {
-    image: require("../../../assets/images/travel/5.jpg"),
+    image: require("../../../assets/images/PC/5.jpg"),
   },
   {
-    image: require("../../../assets/images/travel/6.jpg"),
+    image: require("../../../assets/images/PC/6.jpg"),
   }
 ]
 const { width: screenWidth } = Dimensions.get('window')
 
 const HomeScreens = () => {
-  const router = useRouter( )
+  const router = useRouter()
   const navigation = useNavigation()
-  const {width,height} = useWindowDimensions()
+  const { width, height } = useWindowDimensions()
   const [fontsLoaded, fontError] = useFonts({
     HelvetIns: require("../../../assets/fonts/HelvetIns.ttf"),
     PlaywriteNL: require("../../../assets/fonts/Playwrite_NL/Playwrite-NL.ttf"),
     Montserrat: require("../../../assets/fonts/Montserrat/static/Montserrat-Regular.ttf"),
 
   });
-  const _renderItem = ({item} :  any) => {
+  const _renderItem = ({ item }: any) => {
     return (
-      <TouchableOpacity style={{width:'100%'}}>
-          <Image source = {item.image} style={{width:'100%',height:280, borderRadius:15}} />
+      <TouchableOpacity style={{ width: '100%' }}>
+        <Image source={item.image} style={{ width: '100%', height: 280, borderRadius: 15 }} />
       </TouchableOpacity>
     )
   }
 
   return (
     <SafeAreaView style={styles.box}>
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         {/* {header} */}
         <View style={styles.headerTop}>
           <View>
@@ -94,26 +94,153 @@ const HomeScreens = () => {
         </View>
 
         <View>
-                <View style={{width:'100%'}}>
-                   
-                   {
-                    
-                       <Carousel
-                       sliderWidth={screenWidth}
-                       sliderHeight={screenWidth}
-                       itemWidth={screenWidth - 100}
-                       data={slides}
-                       renderItem={_renderItem}
-                       hasParallaxImages={true}
-                   />
-                   }
-                </View>
-            </View>
+          <View style={{ width: '100%' }}>
 
-      </View>
+            {
+
+              <Carousel
+                sliderWidth={screenWidth}
+                sliderHeight={screenWidth}
+                itemWidth={screenWidth - 100}
+                data={slides}
+                renderItem={_renderItem}
+                hasParallaxImages={true}
+              />
+            }
+          </View>
+        </View>
+
+        {/* category */}
+        <View style={{ width: '100%', padding: 20 }}>
+          {/* Title */}
+          <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+            <FontAwesome5 name="shopping-cart" size={20} color="green" />
+            <Text style={{ fontWeight: 'bold', fontSize: 16, paddingLeft: 10 }}>Our Services</Text>
+          </View>
+
+          {/* Icons and Text */}
+          <View style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+            {[
+              { icon: <FontAwesome name="qrcode" size={30} color="green" />, label: 'QR & Barcode Scanning' },
+              { icon: <FontAwesome name="dollar" size={30} color="green" />, label: 'Price Comparison' },
+              { icon: <FontAwesome name="camera" size={30} color="green" />, label: 'Image Recognition' },
+              { icon: <FontAwesome5 name="bell" size={30} color="green" />, label: 'Stock Alerts' },
+              { icon: <FontAwesome name="heart" size={30} color="green" />, label: 'Recommendations' },
+              { icon: <MaterialIcons name="store" size={30} color="green" />, label: 'Inventory Management' },
+            ].map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                style={{
+                  width: '30%', // Adjust the width as needed
+                  alignItems: 'center',
+                  marginBottom: 20,
+                }}
+              >
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderWidth: 1,
+                    borderColor: '#EBEBEB',
+                  }}
+                >
+                  {item.icon}
+                </View>
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    marginTop: 10,
+                    color: '#000',
+                    fontFamily: 'Montserrat',
+                    fontSize: 12,
+                  }}
+                >
+                  {item.label}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+
+        {/* list PC */}
+
+        <View style={{ width: '100%', paddingHorizontal: 10 }}>
+          <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10 }}>
+            <FontAwesome name="plane" size={30} color="green" />
+            <Text style={{ fontWeight: 'bold', fontSize: 16, paddingVertical: 20, paddingLeft: 10, textTransform: 'capitalize' }}>PC 2024 (hot)</Text>
+          </View>
+          <View style={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+            <TouchableOpacity style={{ width: '50%', padding: 10 }}>
+              <View style={{ width: '100%', backgroundColor: '#F4F4F4', borderRadius: 10 }}>
+                <Image source={require("../../../assets/images/PC/7.jpg")} style={{ width: '100%', height: 120, borderTopRightRadius: 10, borderTopLeftRadius: 10 }} />
+                <View style={{ width: '100%', paddingHorizontal: 20 }}>
+
+                  <Text style={{ fontSize: 16, paddingTop: 15, textAlign: 'left', fontWeight: 'bold', height: 65 }}>Đảo Tuần Châu</Text>
+                  <View style={{ paddingTop: 5, display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+                    <Text style={{ fontSize: 14 }}>4,4</Text>
+                    <FontAwesome name="star" size={16} color="orange" />
+                    <Text style={{ fontSize: 14 }}>(1,3N)</Text>
+                  </View>
+                  <Text style={{ fontSize: 14, paddingVertical: 15 }} numberOfLines={1}>Đảo</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: '50%', padding: 10 }}>
+              <View style={{ width: '100%', backgroundColor: '#F4F4F4', borderRadius: 10 }}>
+                <Image source={require("../../../assets/images/PC/8.jpg")} style={{ width: '100%', height: 120, borderTopRightRadius: 10, borderTopLeftRadius: 10 }} />
+                <View style={{ width: '100%', paddingHorizontal: 20 }}>
+
+                  <Text style={{ fontSize: 16, paddingTop: 15, textAlign: 'left', fontWeight: 'bold', height: 65 }}>Vịnh Bái Tử Long</Text>
+                  <View style={{ paddingTop: 5, display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+                    <Text style={{ fontSize: 14 }}>4,4</Text>
+                    <FontAwesome name="star" size={16} color="orange" />
+                    <Text style={{ fontSize: 14 }}>(1,3N)</Text>
+                  </View>
+                  <Text style={{ fontSize: 14, paddingVertical: 15 }} numberOfLines={1}>Vịnh</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: '50%', padding: 10 }}>
+              <View style={{ width: '100%', backgroundColor: '#F4F4F4', borderRadius: 10 }}>
+                <Image source={require("../../../assets/images/PC/9.jpg")} style={{ width: '100%', height: 120, borderTopRightRadius: 10, borderTopLeftRadius: 10 }} />
+                <View style={{ width: '100%', paddingHorizontal: 20 }}>
+
+                  <Text style={{ fontSize: 16, paddingTop: 15, textAlign: 'left', fontWeight: 'bold', height: 65 }}>Bải Biển Hạ Long</Text>
+                  <View style={{ paddingTop: 5, display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+                    <Text style={{ fontSize: 14 }}>4,4</Text>
+                    <FontAwesome name="star" size={16} color="orange" />
+                    <Text style={{ fontSize: 14 }}>(1,3N)</Text>
+                  </View>
+                  <Text style={{ fontSize: 14, paddingVertical: 15 }} numberOfLines={1}>Điểm thu hút khách nước ngoài</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: '50%', padding: 10 }}>
+              <View style={{ width: '100%', backgroundColor: '#F4F4F4', borderRadius: 10 }}>
+                <Image source={require("../../../assets/images/PC/10.jpg")} style={{ width: '100%', height: 120, borderTopRightRadius: 10, borderTopLeftRadius: 10 }} />
+                <View style={{ width: '100%', paddingHorizontal: 20 }}>
+
+                  <Text style={{ fontSize: 16, paddingTop: 15, textAlign: 'left', fontWeight: 'bold', height: 65 }}>Núi Bài Thơ</Text>
+                  <View style={{ paddingTop: 5, display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+                    <Text style={{ fontSize: 14 }}>4,4</Text>
+                    <FontAwesome name="star" size={16} color="orange" />
+                    <Text style={{ fontSize: 14 }}>(1,3N)</Text>
+                  </View>
+                  <Text style={{ fontSize: 14, paddingVertical: 15 }} numberOfLines={1}>Điểm thu hút khách nước ngoài</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
+
 
 const styles = StyleSheet.create({
   box: {
@@ -144,12 +271,9 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingHorizontal: 20,
     paddingTop: 30
-
-
   },
   boxSearchContent: {
     width: '100%',
-
   },
   searchTitle: {
     fontSize: 30,
